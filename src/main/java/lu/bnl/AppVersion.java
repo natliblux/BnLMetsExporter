1138043a4d305986de87932bd24d02e7a895f3fc
Ralph Marschall <ralph.marschall@bnl.etat.lu>
2019-11-11 15:53:23 +0100

    Change version to 1.7

--- src/main/java/lu/bnl/AppVersion.java
+++ src/main/java/lu/bnl/AppVersion.java
@@ -21,7 +21,7 @@ package lu.bnl;
 public interface AppVersion {
 
 	//Same as pom.version in Maven pom.xml 
-	public static final String VERSION = "1.6.1";
+	public static final String VERSION = "1.7.0";
 	
 	public static final String SOLR_VERSION = "6.6.0";
 }
