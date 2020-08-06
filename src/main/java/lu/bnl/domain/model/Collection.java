9362fc8ed87ddc7c718f1c32bf77f2da2e5fe93d
Ralph Marschall <ralph.marschall@bnl.etat.lu>
2020-08-06 10:29:16 +0200

    Add Collection

---
+++ src/main/java/lu/bnl/domain/model/Collection.java
@@ -0,0 +1,29 @@
+package lu.bnl.domain.model;
+
+public class Collection {
+    
+    private String paperId;
+
+    private String title;
+
+    public Collection() {}
+
+    public String getPaperId() {
+        return paperId;
+    }
+
+    public void setPaperId(String paperId) {
+        this.paperId = paperId;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+
+
+}
